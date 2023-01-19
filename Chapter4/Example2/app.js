bb9713085fc4baa5c1f29cd5827f5e346387483b
ManoSarno <manusarno97@gmail.com>
2023-01-19 11:47:33 -0300

    adding enter key press listener and apply function

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -6,7 +6,24 @@ var main = function () {
     
     if ($(".comment-input input").val() !== "") {
       $new_comment = $("<p>").text($(".comment-input input").val());
+      $new_comment.hide();
       $(".comments").append($new_comment);
+      $new_comment.fadeIn();
+      $(".comment-input input").val("");
+    }
+  });
+
+  $(".comment-input input").on("keypress", function (event) {
+    if (event.keyCode === 13) {
+      var $new_comment;
+    
+      if ($(".comment-input input").val() !== "") {
+        $new_comment = $("<p>").text($(".comment-input input").val());
+        $new_comment.hide();
+        $(".comments").append($new_comment);
+        $new_comment.fadeIn();
+        $(".comment-input input").val("");
+      }
     }
   });
 };
