22373652cf9c2e964f97abe3548f608ef94ee7cd
ManoSarno <manusarno97@gmail.com>
2023-01-19 15:58:54 -0300

    function to change tabs

--- Chapter4/Example3/app.js
+++ Chapter4/Example3/app.js
@@ -1,7 +1,15 @@
 var main = function () {
   "use strict";
   
-  console.log("hello world!");
+  $(".tabs a span").toArray().forEach(function (element) {
+    // create a click handler for this element
+    $(element).on("click", function () {
+      $(".tabs a span").removeClass("active");
+      $(element).addClass("active");
+      $("main .content").empty();
+      return false;
+    });
+  });
 };
   
 $(document).ready(main);
